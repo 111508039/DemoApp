63d4cb93f9dc2219dd3dd1e9773f7bd216035aae
mahesh1103 <kendremaheshkumar12@gmail.com>
2024-03-24 17:56:34 +0530

    Section-1

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Mahesh");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Rohit");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Aditya");
